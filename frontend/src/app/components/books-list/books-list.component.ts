e43bff0b5c8961254630676643f308a2b20612ba
Mükail Askerov <90192553+AskMikk@users.noreply.github.com>
2023-03-17 22:31:55 +0200

    Implement pagination

--- frontend/src/app/components/books-list/books-list.component.ts
+++ frontend/src/app/components/books-list/books-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BookService } from '../../services/book.service';
 import { Book } from '../../models/book';
 import {MatTableDataSource} from "@angular/material/table";
+import {Page, PageRequest} from "../../models/page";
 
 @Component({
   selector: 'app-books-list',
@@ -19,14 +20,44 @@ export class BooksListComponent implements OnInit {
 
   selectedStatus: string = '';
 
+  currentPage: Page<Book> = { content: [], totalElements: 0, number: 0, totalPages: 0 };
+
   ngOnInit(): void {
-    this.bookService.getBooks({}).subscribe((books) => {
-      this.dataSource.data = books.content;
-    });
+    this.loadPage({pageIndex: 0, pageSize: 100});
   }
 
   applyFilter() {
     this.dataSource.filter = this.selectedStatus;
   }
+
+  loadPage(pageRequest: Partial<PageRequest>) {
+    this.bookService.getBooks(pageRequest).subscribe((page) => {
+      this.dataSource.data = page.content;
+      this.currentPage = page;
+    });
+  }
+
+  onLeftArrowClick() {
+    if (this.currentPage && this.currentPage.number > 0) {
+      const pageRequest: Partial<PageRequest> = {
+        pageIndex: this.currentPage.number - 1,
+        pageSize: 100
+      };
+
+      this.loadPage(pageRequest);
+    }
+  }
+
+  onRightArrowClick() {
+    if (this.currentPage && this.currentPage.number < this.currentPage.totalPages - 1) {
+      const pageRequest: Partial<PageRequest> = {
+        pageIndex: this.currentPage.number + 1,
+        pageSize: 100
+      };
+      
+      this.loadPage(pageRequest);
+    }
+  }
+
   displayedColumns: string[] = ['title', 'author', 'genre', 'year', 'added', 'checkOutCount', 'status', 'dueDate', 'comment'];
 }
